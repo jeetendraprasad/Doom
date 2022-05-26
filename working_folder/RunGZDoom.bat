5792cbd0612dbfd7efca93f533db09907f85385b
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2022-05-26 23:42:50 +0530

    changes done

--- working_folder/RunGZDoom.bat
+++ working_folder/RunGZDoom.bat
@@ -3,13 +3,18 @@ SET PATH=D:\Games\Doom\gzdoom-4-7-1-Windows-64bit;%PATH%
 
 ECHO %~dp0
 SET work_folder=%~dp0
+SET work_folder=%work_folder:~0,-1%
 
 PAUSE
 
-gzdoom -nosound -nomusic +vid_fps 1 -config %work_folder%gzdoom.ini -IWAD doom2.wad -file anta_req.zip -file JP_HUD_mod_friendly.pk3 -file "weapons\SuperGun.zip" -file "Textures\DLTP GZDoom Port.pk3"
-@REM -file 3D_Models.pk3
-gzdoom -nosound -nomusic +vid_fps 1 -config %work_folder%gzdoom.ini -file iu-part5-zd109c.zip -file JP_HUD_mod_friendly.pk3 -file "weapons\SuperGun.zip" -file "Textures\DLTP GZDoom Port.pk3"
-gzdoom -nosound -nomusic +vid_fps 1 -config %work_folder%gzdoom.ini -file JP_HUD_mod_friendly.pk3 -file "weapons\SuperGun.zip" -file "Textures\DLTP GZDoom Port.pk3"
+gzdoom -nosound -nomusic +vid_fps 1 -config %work_folder%\gzdoom.ini -file "DoomHDv03.pk3"
+@REM -file JP_HUD_mod_friendly.pk3 -file "SuperGun.zip" -file 3D_Models.pk3
+
+gzdoom -nosound -nomusic +vid_fps 1 -config %work_folder%\gzdoom.ini -file SuperGun.zip -file iu-part5-zd109c.zip -file d64-gfx.zip -file JP_HUD_mod_friendly.pk3 -file "DLTP GZDoom Port.pk3"
+
+exit
+
+gzdoom -nosound -nomusic +vid_fps 1 -config %work_folder%\gzdoom.ini -file JP_HUD_mod_friendly.pk3 -file "SuperGun.zip" -file "DLTP GZDoom Port.pk3"
 
 PAUSE
 EXIT
